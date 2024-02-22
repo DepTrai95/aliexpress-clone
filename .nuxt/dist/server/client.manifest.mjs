67810aa7c3d31fa43b4c1376541d8a390b6b8953
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-02-22 22:36:19 +0100

    remove nuxt-folder and add it to gitignore

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/nguyen/Sites/aliexpress-clone/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/nguyen/Sites/aliexpress-clone/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
