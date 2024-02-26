a726579f16ea0aee5418788a0bff365a1df4969a
Duc Anh Nguyen <53148787+DepTrai95@users.noreply.github.com>
2024-02-26 23:40:18 +0100

    [TASK] finalize supabase/prisma frontend

--- server/api/prisma/add-address.js
+++ server/api/prisma/add-address.js
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
+    const body = await readBody(event);
     
     const res = await prisma.addresses.create({
         data: {
--- server/api/prisma/get-address-by-user/[id].js
+++ server/api/prisma/get-address-by-user/[id].js
@@ -1,18 +1,9 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    
-    const res = await prisma.addresses.update({
-        where: { id: Number(event.context.params.id) },
-        data: {
-            name: body.name,
-            address: body.address,
-            zipcode: body.zipCode,
-            city: body.city,
-            country: body.country,
-        }
+    const res = await prisma.addresses.findFirst({
+        where: { userId: event.context.params.id }
     })
 
     return res
